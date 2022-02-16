c9bc72f19548945435d827d1b475d7a27d02a40f
mikgrinkevich <85791543+mikgrinkevich@users.noreply.github.com>
2022-02-17 01:57:46 +0300

    game functionality added

---
+++ CleanetCode.TypesLoopsBranches/CleanetCode.GuessingNumberGame/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter your nickname");
+string? userName = Console.ReadLine();
+Console.WriteLine($"Please, {userName}, guess my number (0 to 1000)");
+
+//creation of random generating value
+Random randomValueToGuess = new Random();
+int secretNumber = randomValueToGuess.Next(0,1000);
+
+bool isWin = false;
+int userNumber = -1;
+bool isIntNumber;
+
+int attemptCount = 0;
+while (!isWin) //running till expression in the brackets wouldn't become true
+{
+    do
+    {
+        Console.WriteLine($"Please, {userName}, make a guess!");
+        string? userInput = Console.ReadLine();
+        isIntNumber = int.TryParse(userInput, out userNumber);
+
+        attemptCount++;
+
+        if (!isIntNumber)
+        {
+            Console.WriteLine($"Your input value {userInput} is incorrect");
+        }
+    } while (!isIntNumber);
+
+    if (userNumber > secretNumber)
+    {
+        Console.WriteLine($"Your number ({userNumber}) is larger than assumed");
+    }
+    else if (userNumber < secretNumber)
+    {
+        Console.WriteLine($"Your number ({userNumber}) is less than assumed");
+    }
+    else
+    {
+        Console.WriteLine("You have won!" +
+            $" Thank you, {userName}, your number of attempts is {attemptCount}");
+        break;
+    }
+}
---
+++ CleanetCode.TypesLoopsBranches/CleanetCode.TypesLoopsBranches/Program.cs
@@ -0,0 +1,17 @@
+﻿//literals and variables
+Console.WriteLine(1L);
+var i = 21;
+i.ToString();
+var j = 19.0M;
+j.ToString();
+int testValue = 31;
+Console.WriteLine(3.2e3);
+Console.WriteLine(1.2E-1);
+
+double sumCheckX = 0.1;
+double sumCheckY = 0.2;
+Console.WriteLine(sumCheckX + sumCheckY);
+
+decimal decimalCheckX = 0.1M;
+decimal decimalCheckY = 0.2M;
+Console.WriteLine(decimalCheckY + decimalCheckX);
